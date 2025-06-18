b3db4e657e27eacab4f6a6ea8c0d97cb732412af
Vlad Mocanu <vladmocanu2003@gmail.com>
2025-06-18 17:02:09 +0300

    [Backend] Changed one letter

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'Production') {
 }
 const { Sequelize, DataTypes } = require('sequelize');
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'Production';
 
 console.log("Database config being used:");
 console.log("Host:", process.env.DB_HOST);
